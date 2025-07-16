1af20ae8e1a35b8390a5e2718973d742303826bf
Tenny <tenny_lu@phison.com>
2025-07-16 18:47:17 +0800

    Add methods to split text chunks.

--- LMBackend/DocumentSplitter.cs
+++ LMBackend/DocumentSplitter.cs
@@ -1,13 +1,63 @@
-﻿namespace LMBackend;
+﻿using System.Text;
+using UglyToad.PdfPig;
+using Xceed.Words.NET;
+
+namespace LMBackend;
 
 public static class DocumentSplitter
 {
     public static List<string> GetLines(string name, byte[] data)
     {
-        List<string> lines = new List<string>();
-        return lines;
+        // Get extension from name
+        FileInfo file = new FileInfo(name);
+        switch (file.Extension)
+        {
+            case ".pdf":
+                return GetLinesFromPdf(data);                
+            case ".txt":
+                return GetLinesFromTxt(data);
+            case ".docx":
+                return GetLinesFromDocx(data);
+            default:
+                throw new NotSupportedException("Unsupported type: " + file.Extension);                
+        }
     }
 
+    private static List<string> GetLinesFromTxt(byte[] data)
+    {
+        string text = Encoding.UTF8.GetString(data);
+        text = text.Trim();
+        return text.Split().ToList();
+    }
+
+    private static List<string> GetLinesFromPdf(byte[] data)
+    {
+        try
+        {
+            using PdfDocument pdf = PdfDocument.Open(data);
+            return pdf.GetPages().Select(p => p.Text).ToList();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Failed to get text from PDF: " + ex);
+            return null;
+        }
+    }
+
+    private static List<string> GetLinesFromDocx(byte[] data)
+    {
+        try
+        {
+            using MemoryStream ms = new MemoryStream(data);
+            using DocX docx = DocX.Load(ms);
+            return docx.Paragraphs.Select(x => x.Text).ToList();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Failed to get text from DOCX: " + ex);
+            return null;
+        }
+    }
 
     public static List<string> SplitText(List<string> units, int maxChunkWords = 250, int overlapWords = 50)
     {
