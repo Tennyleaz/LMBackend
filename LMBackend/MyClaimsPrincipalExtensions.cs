f71888f9e9e8ebdce53515dfb690eb924207e72b
Tenny <tenny_lu@phison.com>
2025-07-21 18:15:16 +0800

    Fix user id when claim is empty.

--- LMBackend/MyClaimsPrincipalExtensions.cs
+++ LMBackend/MyClaimsPrincipalExtensions.cs
@@ -6,11 +6,18 @@ namespace LMBackend
     {
         public static Guid GetUserId(this ClaimsPrincipal user)
         {
+            if (user == null)
+                return Guid.Empty;
+
             // In my JWT, the user id is stored in the subject of the claim. Also check NameIdentifier just to be safe.
             string userId = user.FindFirst(ClaimTypes.NameIdentifier)?.Value
                          ?? user.FindFirst("sub")?.Value;
 
-            return userId != null ? Guid.Parse(userId) : Guid.Empty;
+            if (string.IsNullOrEmpty(userId))
+                return Guid.Empty;
+            if (Guid.TryParse(userId, out Guid parsed))
+                return parsed;
+            return Guid.Empty;
         }
     }
 }
