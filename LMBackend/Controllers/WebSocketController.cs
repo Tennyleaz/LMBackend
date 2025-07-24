477c4cd3248c12c9f3db269f0008c585901065b9
Tenny <tenny_lu@phison.com>
2025-07-24 11:47:12 +0800

    Try to fix audio queue.

--- LMBackend/Controllers/WebSocketController.cs
+++ LMBackend/Controllers/WebSocketController.cs
@@ -16,6 +16,9 @@ public class WebSocketController : Controller
 {
     private readonly ISttService _sttService;
     private readonly ConcurrentQueue<SttChunk> wavFileQueue = new ConcurrentQueue<SttChunk>();
+    private const int SLICE_SEC = 1;
+    private const string INPUT_TYPE = "webm";
+    private const int MAX_COMBINE = 5;
 
     public WebSocketController(ISttService sttService)
     {
@@ -38,13 +41,14 @@ public class WebSocketController : Controller
 
     private async Task HandleAudioWebSocket(WebSocket socket)
     {
-        string chunkDir = @"D:\test";
+        string chunkDir = @"C:\temp audio";
         Directory.CreateDirectory(chunkDir);
 
         using MemoryStream bufferStream = new MemoryStream();
         byte[] buffer = new byte[8192];
 
         bool isStopped = false;
+        bool isQueueStopped = false;
         bool isPaused = false;
         Task transcriptionTask = Task.Run(async () =>
         {
@@ -58,8 +62,8 @@ public class WebSocketController : Controller
                         IAsyncEnumerable<SegmentData> datas = _sttService.WhisperChunk(chunk.File);
                         await foreach (SegmentData data in datas)
                         {
-                            double start = chunk.Start + data.Start.TotalSeconds;
-                            double end = chunk.Start + data.End.TotalSeconds;
+                            double start = chunk.Start * SLICE_SEC + data.Start.TotalSeconds;
+                            double end = chunk.Start * SLICE_SEC + data.End.TotalSeconds;
                             await SendTranscript(socket, data.Text, start, end, chunk.IsLast);
                         }
                         if (chunk.IsLast)
@@ -75,10 +79,17 @@ public class WebSocketController : Controller
                 }
                 else
                 {
-                    //Console.WriteLine("transcriptionTask nothing to dequeue.");
+                    if (isQueueStopped)
+                    {
+                        Console.WriteLine("transcriptionTask isQueueStopped");
+                        break;
+                    }
                 }
                 await Task.Delay(1000); // Check every 1s
             }
+            Console.WriteLine("transcriptionTask stopped");
+            // Tell user to close again
+            await SendTranscript(socket, null, 0, 0, true);
         });
 
         int fileCounter = 0;
@@ -90,10 +101,18 @@ public class WebSocketController : Controller
                 while (true)
                 {
                     // Only combine if current index > counter + 10, or is paused/stopped
-                    if (fileCounter - currentFileIndex > 10 || isPaused || isStopped)
+                    if (fileCounter - currentFileIndex > MAX_COMBINE || isPaused || isStopped)
                     {
+                        // Check if no files
+                        if (fileCounter == currentFileIndex)
+                        {
+                            if (isStopped)
+                                break;
+                            if (isPaused)
+                                continue;
+                        }
                         // Take the 1 file before current, to overlap
-                        int start = Math.Max(currentFileIndex - 1, 0);
+                        int start = Math.Max(currentFileIndex - 1, 1);  // Started at 1
                         if (start >= fileCounter)
                         {
                             // No file to combine
@@ -117,34 +136,14 @@ public class WebSocketController : Controller
                                 End = fileCounter,
                                 IsLast = isStopped
                             };
-                            wavFileQueue.Enqueue(chunk);
-                            currentFileIndex = fileCounter;  // Advance after enqueueing
+                            wavFileQueue.Enqueue(chunk);                            
                         }
                         catch (Exception ex)
                         {
                             Console.WriteLine(ex);
                         }
+                        currentFileIndex = fileCounter;  // Advance after enqueueing
                     }
-                    //else if (isStopped)
-                    //{
-                    //    // Last files % 5 < 5
-                    //    int remain = fileCounter % 5;
-                    //    if (remain > 0)
-                    //    {
-                    //        int start = fileCounter - remain;
-                    //        string combinedFile = CombineAudioFiles(chunkDir, start, fileCounter);
-                    //        Console.WriteLine("Created wav file: " + combinedFile);
-                    //        SttChunk chunk = new SttChunk
-                    //        {
-                    //            File = combinedFile,
-                    //            Start = start,
-                    //            End = fileCounter,
-                    //            IsLast = isStopped
-                    //        };
-                    //        wavFileQueue.Enqueue(chunk);
-                    //    }
-                    //    break;
-                    //}
                     await Task.Delay(1000); // Check every second
                 }
             }
@@ -152,6 +151,8 @@ public class WebSocketController : Controller
             {
                 Console.WriteLine(ex);
             }
+            isQueueStopped = true;
+            Console.WriteLine("combineTask stopped");
         });
 
         try
@@ -171,16 +172,13 @@ public class WebSocketController : Controller
                 {
                     if (bufferStream.Length > 0)
                     {
-                        string fileName = $"chunk_{fileCounter++}.webm";
+                        fileCounter++;  // File count starts at 1
+                        string fileName = $"chunk_{fileCounter}.{INPUT_TYPE}";
                         fileName = Path.Combine(chunkDir, fileName);
                         await System.IO.File.WriteAllBytesAsync(fileName, bufferStream.ToArray());
-                        bufferStream.SetLength(0); // Clear for next chunk
-                        //Console.WriteLine($"Write {result.Count} bytes from websocket");
+                        bufferStream.SetLength(0);  // Clear for next chunk                                                   
                         //Console.WriteLine($"Write audio file {fileCounter} from websocket");
                     }
-
-                    // append audio after every 5s, assume each chunk is 1s long
-
                 }
                 else if (result.MessageType == WebSocketMessageType.Close)
                 {
@@ -209,16 +207,21 @@ public class WebSocketController : Controller
         }
         finally
         {
+            Console.WriteLine("Websocket handler leave...");
             await transcriptionTask;
             await combineTask;
             transcriptionTask.Dispose();
             combineTask.Dispose();
+            Console.WriteLine("Websocket handler leave... 1");
             wavFileQueue.Clear();
+            Console.WriteLine("Websocket handler leave... 2");
             if (socket.State == WebSocketState.Open)
             {
                 await socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Done", CancellationToken.None);
             }
-            Console.WriteLine("Websocket handler leave.");
+            Console.WriteLine("Websocket handler leave... 3");
+            ClearDir(chunkDir);
+            Console.WriteLine("Websocket handler leave... done.");
         }
     }
 
@@ -246,8 +249,13 @@ public class WebSocketController : Controller
         List<string> files = new List<string>();
         for (int i=start; i<=end; i++)
         {
-            string theFile = $"chunk_{i}.webm";
+            string theFile = $"chunk_{i}.{INPUT_TYPE}";
             theFile = Path.Combine(baseDir, theFile);
+            if (!System.IO.File.Exists(theFile))
+            {
+                Console.WriteLine("File not exist! " + theFile);
+                continue;
+            }
             string line = $"file '{theFile}'";
             files.Add(line);
         }
@@ -257,10 +265,11 @@ public class WebSocketController : Controller
         // ffmpeg -f concat -safe 0 -i mylist.txt -c copy output.wav
         string outputName = $"output_{start}-{end}.wav";
         outputName = Path.Combine(baseDir, outputName);
+        string arguments = $"-f concat -safe 0 -i \"{concatFileName}\" -ar 16000 -acodec pcm_s16le -ac 1 \"{outputName}\"";
         ProcessStartInfo psi = new ProcessStartInfo
         {
             FileName = "ffmpeg",
-            Arguments = $"-f concat -safe 0 -i {concatFileName} -ar 16000 -acodec pcm_s16le -ac 1 {outputName}",
+            Arguments = arguments,
             RedirectStandardOutput = true,
             RedirectStandardError = true,
             UseShellExecute = false,
@@ -270,6 +279,7 @@ public class WebSocketController : Controller
         process.WaitForExit();
         if (process.ExitCode != 0)
         {
+            Console.WriteLine("Error args: " + arguments);
             string error = process.StandardError.ReadToEnd();
             throw new Exception($"Command failed: {error}");
         }
@@ -307,5 +317,21 @@ public class WebSocketController : Controller
         public double end { get; set; }
         public string text { get; set; }
         public bool isStopped { get; set; }
+    }
+
+    private static void ClearDir(string dir)
+    {
+        if (!Directory.Exists(dir))
+            return;
+
+        string[] files = Directory.GetFiles(dir);
+        foreach (string file in files)
+        {
+            try
+            {
+                System.IO.File.Delete(file);
+            }
+            catch { }
+        }
     }
 }
