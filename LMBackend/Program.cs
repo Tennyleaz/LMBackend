a46fca40ad6946323f2bd0bbe3271f8a0a27c52e
Tenny <tenny_lu@phison.com>
2025-08-18 11:10:43 +0800

    Always use swagger.

--- LMBackend/Program.cs
+++ LMBackend/Program.cs
@@ -144,7 +144,7 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
