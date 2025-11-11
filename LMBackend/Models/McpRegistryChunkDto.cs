14125e33398a351fd82bf7588af29e201ae807f4
Tenny <tenny_lu@phison.com>
2025-11-11 15:06:05 +0800

    fix: Fix MCP chunk to string embedding.

--- LMBackend/Models/McpRegistryChunkDto.cs
+++ LMBackend/Models/McpRegistryChunkDto.cs
@@ -5,4 +5,10 @@ public class McpRegistryChunkDto
     public string Name { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
+
+    public override string ToString()
+    {
+        string result = $"Name: {Name},\nTitle: {Title}\nDescription: {Description}";
+        return result;
+    }
 }
